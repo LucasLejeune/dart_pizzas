40372cefcd1028ea94b2f83cc678d674ce10f0dd
LucasLejeune <lucaslejeune0@gmail.com>
2024-09-02 14:14:06 +0200

    refactor main

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -2,17 +2,16 @@ import 'package:dart_pizzas/src/ingredient.dart';
 import 'package:dart_pizzas/src/pizza.dart';
 
 void main() {
-  final tomate = new Ingredient('Tomate', true);
-  final jambon = new Ingredient('jambon', false);
-  final fromage = new Ingredient('fromage', false);
-  final champignon = new Ingredient('champignon', true);
+  final tomate = Ingredient('Tomate', true);
+  final jambon = Ingredient('jambon', false);
+  final fromage = Ingredient('fromage', false);
+  final champignon = Ingredient('champignon', true);
+  List<Pizza> pizzas = [];
+  pizzas.add(Pizza(nom: 'Vegan', ingredients: [tomate, champignon]));
+  pizzas.add(Pizza(nom: 'Basique', ingredients: [tomate, champignon, jambon]));
+  pizzas.add(Pizza(nom: 'Fromage', ingredients: [tomate, fromage]));
 
-  final pizza1 = new Pizza(nom: 'Vegan', ingredients: [tomate, champignon]);
-  final pizza2 =
-      new Pizza(nom: 'Basique', ingredients: [tomate, champignon, jambon]);
-  final pizza3 = new Pizza(nom: 'Fromage', ingredients: [tomate, fromage]);
-
-  print('Pizza ${pizza1.nom}: ${pizza1.isVegan() ? 'vegan' : 'non-vegan'}');
-  print('Pizza ${pizza2.nom}: ${pizza2.isVegan() ? 'vegan' : 'non-vegan'}');
-  print('Pizza ${pizza3.nom}: ${pizza3.isVegan() ? 'vegan' : 'non-vegan'}');
+  for (Pizza pizza in pizzas) {
+    print('Pizza ${pizza.nom}: ${pizza.isVegan() ? 'vegan' : 'non-vegan'}');
+  }
 }
